0155a9930be7792adc14356d1c523def6837e014
Benjamín Isasmendi C <isasmendi223@gmail.com>
2024-10-05 18:56:32 -0300

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
-
 
 function App() {
     return (
-      <Router>
-          <Route exact path="pages/" component={Home} />
-      </Router>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
     );
-
 }
 
 export default App;
+
--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-    document.getElementById('root')
 )
